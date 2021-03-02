e580ac153fa64f7eedd0c7aa06dca25aa820f5ae
nmenag <nmena.garzon@gmail.com>
2021-03-02 11:52:32 -0500

    Add storeChannel

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :channels_project, ChannelsProject.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "hainover1",
   database: "channels_project_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- lib/channels_project_web/channels/user_socket.ex
+++ lib/channels_project_web/channels/user_socket.ex
@@ -21,8 +21,10 @@ defmodule ChannelsProjectWeb.UserSocket do
   channel "stores:*", ChannelsProjectWeb.StoresChannel
 
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"userId" => raw_user_id}, socket, _connect_info) do
+    Logger.info("I AM READY FUCK YOU!!!!!....")
+
+    {:ok, assign(socket, user_id: raw_user_id)}
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
